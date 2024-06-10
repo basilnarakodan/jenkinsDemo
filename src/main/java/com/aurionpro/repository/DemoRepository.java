d51a4ddcd75385bff9dd1c48399a61d63de5fda0
Basil <basil.narakodan@aurionpro.com>
2024-06-10 16:40:38 +0530

    a

--- src/main/java/com/aurionpro/repository/DemoRepository.java
+++ src/main/java/com/aurionpro/repository/DemoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 public class DemoRepository {
 
 	public int sum(int number1,int number2) {
-		return number1-number2;
+		return number1*number2;
 	}
 }
